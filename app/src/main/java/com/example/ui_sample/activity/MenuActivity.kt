fbcef6c9f733d0acd2aec01d9d3646a712053dbe
LeeJaeWon <zhtm2@openobject.net>
2021-10-13 14:43:47 +0900

    - option menu 수정, - popup menu 추가

--- app/src/main/java/com/example/ui_sample/activity/MenuActivity.kt
+++ app/src/main/java/com/example/ui_sample/activity/MenuActivity.kt
@@ -7,6 +7,7 @@ import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.PopupMenu
 import android.widget.Toast
 import com.example.ui_sample.DialogSampleFragment
 import com.example.ui_sample.R
@@ -28,8 +29,28 @@ class MenuActivity : AppCompatActivity() {
         setSupportActionBar(binding.tbrToolbar)
 
         registerForContextMenu(binding.btnMenu)
+        binding.btnMenu.setOnClickListener {
+            val popup = PopupMenu(this, it)
+            menuInflater.inflate(R.menu.context_menu, popup.menu)
+            popup.setOnMenuItemClickListener {
+                when(it.itemId) {
+                    R.id.context_first -> Toast.makeText(this, "first", Toast.LENGTH_SHORT).show()
+                    R.id.context_second -> Toast.makeText(this, "second", Toast.LENGTH_SHORT).show()
+                }
+                return@setOnMenuItemClickListener true
+            }
+            popup.show()
+        }
     }
 
+    /*
+        showAsAction 속성
+        - always : 항상 액션바에 표시
+        - never : 액션바에 표시하지 않음
+        - iRoom : 공간에 여유가 있으면 밖에 표시
+        - withText : 아이콘이 있을 때 글자와 함께 표시
+        - collapseActionView : SearchView 등과 함께 사용
+     */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true
@@ -42,6 +63,9 @@ class MenuActivity : AppCompatActivity() {
             }
             R.id.second -> {
                 Toast.makeText(this, "second", Toast.LENGTH_SHORT).show()
+            }
+            R.id.third -> {
+                Toast.makeText(this, "third", Toast.LENGTH_SHORT).show()
             }
         }
 
