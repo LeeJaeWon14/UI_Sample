5733e507c2d82dd85863abb2b26ccb2b749da9fc
LeeJaeWon <zhtm2@openobject.net>
2021-11-01 15:25:11 +0900

    - will implementing popupWindow above keyboard

---
+++ app/src/main/java/com/example/ui_sample/activity/KeyboardActivity.kt
@@ -0,0 +1,17 @@
+package com.example.ui_sample.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ui_sample.R
+import com.example.ui_sample.databinding.ActivityKeyboardBinding
+
+class KeyboardActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityKeyboardBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityKeyboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val popupView = layoutInflater.inflate()
+    }
+}
